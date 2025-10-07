7ef2bd3d47d1e71c50292c76512af1b23e81de98
McAdams <BZ317@UNUM.COM>
2025-10-07 03:16:30 -0400

    Enhancement three is complete

--- app_admin/src/app/models/user.ts
+++ app_admin/src/app/models/user.ts
@@ -1,10 +1,30 @@
-export class User {
-    email: string;
-    name: string;
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-    constructor ()
-    {
-        this.email = '';
-        this.name = '';
-    }
-}
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+@.+\..+/
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;

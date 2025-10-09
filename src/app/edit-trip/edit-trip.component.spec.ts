bb96f5ff0499b1df03a332d943ce2fc6f85acd72
Jessie McAdams <angrytealeaf@gmail.com>
2025-10-08 23:43:19 -0400

    test: fix specs and app template to match current components/services

--- src/app/edit-trip/edit-trip.component.spec.ts
+++ src/app/edit-trip/edit-trip.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { EditTripComponent } from './edit-trip.component';
 
@@ -8,7 +10,7 @@ describe('EditTrip', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditTripComponent]
+      imports: [EditTripComponent, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
 
